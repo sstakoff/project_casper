93e898434f55bc88cd28f8a7b1338059848f3793
sstakoff <sstakoff@yahoo.com>
2022-10-21 09:09:31 -0400

    wip

--- main/project_casper.cpp
+++ main/project_casper.cpp
@@ -8,15 +8,16 @@ extern "C" void app_main(void)
   App a;
   
   WifiMgr w(NULL);
+  // w.provision("abc", true);
+  // ESP_LOGI("app", "Connected!!!");
+
+  // vTaskDelay(5000 / portTICK_PERIOD_MS);
+  // ESP_LOGI("app","Starting reprovision");
   w.provision("abc", true);
-  ESP_LOGI("app", "Connected!!!");
-
-  vTaskDelay(5000 / portTICK_PERIOD_MS);
-  ESP_LOGI("app","Starting reprovision");
-  w.provision("abc", true);
 
 
-
-  ESP_LOGI("app", "Connected!!!");
+  for (;;) {
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
 
 }
